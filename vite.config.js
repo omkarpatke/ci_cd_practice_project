f644fde87d1a05c41b48f9a64d94d5c18dd8b988
omkarpatke <omkarpatke1101@gmail.com>
2026-08-11 17:53:22 +0530

    correct base project name

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/ci-cd-practice-project/',
+  base: '/ci_cd_practice_project/',
   plugins: [react()],
 })
